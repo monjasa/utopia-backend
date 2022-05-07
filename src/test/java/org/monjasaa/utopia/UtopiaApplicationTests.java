03b25d96db5a901f2e6b55ae42dc38800909ac36
monjasa <monja.nya@gmail.com>
2022-05-07 17:41:47 +0300

    Initial commit

---
+++ src/test/java/org/monjasaa/utopia/UtopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.monjasaa.utopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
